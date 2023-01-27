139b38a837d1263021f0d1dde0aa4eee11577a1b
Tendry Alex <tendry1234@gmail.com>
2023-01-27 05:05:36 +0300

    try-solving-conflict

--- front-end/front-end-garage/src/app/app.module.ts
+++ front-end/front-end-garage/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -6,10 +7,16 @@ import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app.routing';
 import { ComponentsModule } from './components/components.module';
 import { AppComponent } from './app.component';
-import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+//import { SignUpComponent } from './sign-up/sign-up.component';
+//import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderHomeComponent } from './header-home/header-home.component';
+import { CarouselHomeComponent } from './carousel-home/carousel-home.component';
+import { HomeBodyComponent } from './home-body/home-body.component';
 
 @NgModule({
   imports: [
+    BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
@@ -20,8 +27,12 @@ import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.compon
   ],
   declarations: [
     AppComponent,
-    AdminLayoutComponent,
-
+    //SignUpComponent,
+    //LoginComponent,
+    HomeComponent,
+    HeaderHomeComponent,
+    CarouselHomeComponent,
+    HomeBodyComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
