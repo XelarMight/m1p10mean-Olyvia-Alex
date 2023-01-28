d912f4855bb6fbc0db68dd44246437a134746c6f
OlyviaHasina <olyviahasina.razakamanantsoa@gmail.com>
2023-01-28 22:27:15 +0100

    Revert "update front"
    This reverts commit a4ca7a10fb8064fb2f1642d623451095f4a6123c.

--- front-end/front-end-garage/src/app/header-home/header-home.component.ts
+++ front-end/front-end-garage/src/app/header-home/header-home.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header-home',
   templateUrl: './header-home.component.html',
   styleUrls: ['./header-home.component.css']
 })
-export class HeaderHomeComponent implements OnInit {
-  logo: string = "assets/template-assets/img/logo.png";
+export class HeaderHomeComponent {
 
-  constructor() {}
-
-  ngOnInit(): void {
-  }
 }
