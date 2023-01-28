b5d939aefd1837ede6932ea3568eaf1515c6d2d0
XelarMight <tendry1234@gmail.com>
2023-01-28 08:11:47 +0300

    changed front up

--- front-end/front-end-garage/src/app/app.module.ts
+++ front-end/front-end-garage/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HomeBodyComponent } from './home-body/home-body.component';
+import { HeaderHomeComponent } from './header-home/header-home.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { CarouselHomeComponent } from './carousel-home/carousel-home.component';
+import { FooterHomeComponent } from './footer-home/footer-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HomeBodyComponent,
+    HeaderHomeComponent,
+    LoginComponent,
+    SignUpComponent,
+    CarouselHomeComponent,
+    FooterHomeComponent
   ],
   imports: [
     BrowserModule,
