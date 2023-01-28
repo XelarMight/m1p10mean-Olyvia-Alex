a4ca7a10fb8064fb2f1642d623451095f4a6123c
OlyviaHasina <olyviahasina.razakamanantsoa@gmail.com>
2023-01-28 22:18:59 +0100

    update front

--- front-end/front-end-garage/src/app/header-home/header-home.component.ts
+++ front-end/front-end-garage/src/app/header-home/header-home.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header-home',
   templateUrl: './header-home.component.html',
   styleUrls: ['./header-home.component.css']
 })
-export class HeaderHomeComponent {
+export class HeaderHomeComponent implements OnInit {
+  logo: string = "assets/template-assets/img/logo.png";
 
+  constructor() {}
+
+  ngOnInit(): void {
+  }
 }
